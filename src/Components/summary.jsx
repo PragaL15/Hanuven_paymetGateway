63b50678d1d3855a539c6010ced13930532e8308
Pragalya Kanakaraj <pragalyakanakaraj@gmail.com>
2024-11-17 15:32:09 +0530

    updated functionality

--- src/Components/summary.jsx
+++ src/Components/summary.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
-const Summary = ({ calculateTotal }) => {
+const Summary = ({ calculateTotal,totalQuantity }) => {  //use only the small case while using state from other component
   const navigate = useNavigate();
 
   return (
@@ -12,12 +12,9 @@ const Summary = ({ calculateTotal }) => {
 
       <div className="grid grid-cols-2 gap-y-5 border-b pb-4">
         <span className="text-gray-600">Total Quantity</span>
-        <span className="text-gray-800 font-medium ml-24">0</span>
+        <span className="text-gray-800 font-medium ml-24">{totalQuantity}</span>
 
         <span className="text-gray-600">Delivery Charge</span>
-        <span className="text-gray-800 font-medium ml-24">₹0</span>
-
-        <span className="text-gray-600">Price of Each Item</span>
         <span className="text-gray-800 font-medium ml-24">₹0</span>
 
         <span className="text-gray-600">Discount</span>
